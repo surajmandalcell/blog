38882f8805ae902c7ee9a197fbdddecffabbe9b5
DESKTOP-MINISLQ\surajmandalcell <surajmandalcell@gmail.com>
2017-02-16 03:31:07 +0530

    //update fonts and about page

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+function showDesigner() {
+    var div = document.getElementById("designer");
+    if (div.style.display !== "none") {
+        div.style.display = "none";
+    } else {
+        div.style.display = "block";
+    }
+}
+
+function closeDeg() {
+    var div = document.getElementById("designer");
+    if (div.style.display !== "block") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
+
+function showProgrammer() {
+    var div = document.getElementById("programmer");
+    if (div.style.display !== "none") {
+        div.style.display = "none";
+    } else {
+        div.style.display = "block";
+    }
+}
+
+function closePog() {
+    var div = document.getElementById("programmer");
+    if (div.style.display !== "block") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
+
